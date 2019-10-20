f682c3d30a300885f2ea50962a272cd2f75598d2
Quang Nghĩa <tonystrinh@gmail.com>
2019-10-20 23:19:35 +0700

    enabling cors 2

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ require('./middlewares/passport');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+app.configure(() => {
+  app.use(allowCrossDomain)
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
