81047edd9957b6905b8ee1c72d8af7f5a97625ee
Quang Nghĩa <tonystrinh@gmail.com>
2019-10-21 22:39:17 +0700

    allow authorization

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   next();
 }
 app.use(allowCrossDomain);
