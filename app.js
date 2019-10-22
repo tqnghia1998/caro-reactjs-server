9acc99995630f38039d13c17ea0b649d1e60cf0b
Quang Nghĩa <tonystrinh@gmail.com>
2019-10-22 10:36:10 +0700

    fix author 8

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var app = express();
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Accept-Language,Origin,X-Requested-With,Content-Type,Accept,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   next();
 }
 app.use(allowCrossDomain);
--- routes/profile.js
+++ routes/profile.js
@@ -23,4 +23,24 @@ router.get('/me', (req, res, next) => {
     }
 });
 
+// these are super bad codes, ask for expertises later
+router.options('/me', (req, res, next) => {
+    
+    // for security, do not send password
+    if (req.user.length > 0) {
+        const user = req.user[0];
+        const info = {
+            username: user.username,
+            email: user.email,
+            fullname: user.fullname
+        }
+        res.status(200).json(info);
+    }
+    else {
+        res.status(400).json({
+            message: 'Đã xảy ra lỗi, vui lòng thử lại'
+        })
+    }
+});
+
 module.exports = router;
