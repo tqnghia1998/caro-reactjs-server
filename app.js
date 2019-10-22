7173bb60b84fc5d2c3c643a479645584c97922ef
Quang Nghĩa <tonystrinh@gmail.com>
2019-10-22 10:42:40 +0700

    debug author 1

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS'){
+    return res.status(200).json({});
+}
   next();
 }
 app.use(allowCrossDomain);
