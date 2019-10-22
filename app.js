7e2f9f44876ba6c935f355e68f17426ca993aa52
Quang Nghĩa <tonystrinh@gmail.com>
2019-10-22 09:46:36 +0700

    fix author 3

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Accept-Language,Origin,X-Requested-With,Content-Type,Accept,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   next();
 }
