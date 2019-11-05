46a7832cec6fe7fe94add2f1dfd612395751c282
Quang Nghĩa <tonystrinh@gmail.com>
2019-11-05 22:23:19 +0700

    update config file

--- config.js
+++ config.js
@@ -1,21 +1,31 @@
-// module.exports = {
-//     'client-domain': '//localhost:6969/',
-//     'server-domain': '//localhost:3000/',
-//     'database': {
-//         'host': 'sql12.freemysqlhosting.net',
-//         'port': '3306',
-//         'user': 'sql12308370',
-//         'password': 'xLicY5Js7i',
-//         'database': 'sql12308370'
-//         // 'host': 'localhost',
-//         // 'port': '3306',
-//         // 'user': 'root',
-//         // 'password': 'admin',
-//         // 'database:' 'web-btcn06-1612422'
-//     }
-// }
+// these exports are for localhost and database mysql local
+var exports_1 = {
+    'client-domain': '//localhost:6969/',
+    'server-domain': '//localhost:3000/',
+    'database': {
+        'host': 'localhost',
+        'port': '3306',
+        'user': 'root',
+        'password': 'admin',
+        'database': 'web-btcn06-1612422'
+    }
+}
 
-module.exports = {
+// these exports are for localhost and database remote
+var exports_2 = {
+    'client-domain': '//localhost:6969/',
+    'server-domain': '//localhost:3000/',
+    'database': {
+        'host': 'sql12.freemysqlhosting.net',
+        'port': '3306',
+        'user': 'sql12308370',
+        'password': 'xLicY5Js7i',
+        'database': 'sql12308370'
+    }
+}
+
+// these exports are for uploading to heroku
+var exports_3 = {
     'client-domain': 'https://btgk-1612422.herokuapp.com/',
     'server-domain': 'https://btcn06-1612422.herokuapp.com/',
     'database': {
@@ -26,3 +36,5 @@ module.exports = {
         'database': 'sql12308370'
     }
 }
+
+module.exports = exports_1;
